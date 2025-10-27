3242832492dfdccb7da596cb9295d84d95148ec4
sudek <sudek@SUDE>
2025-10-28 00:10:30 +0300

    Proje dosyası ekle.

---
+++ Calculator.Tests/CalculatorTests.cs
@@ -0,0 +1,83 @@
+using NUnit.Framework;
+using Project.Calculator.Core;
+
+namespace Project.Calculator.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [TestCase(2,3,5,TestName = "Add_PositiveNumbers")]
+        [TestCase(-5, -10, -15, TestName = "Add_NegativeNumbers")]
+        [TestCase(10.5, 2.5, 13.0, TestName = "Add_DecimalNumbers")]
+        [TestCase(100, 0, 100, TestName = "Add_WithZero")]
+        public void Should_GiveRightAnswer_When_AddDifferentInputs(double a, double b, double expectedResult)
+        {
+            // Arrange
+            var calculator = new Project.Calculator.Core.Calculator();
+
+            // Act
+            var result = calculator.Add(a, b);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expectedResult), $"Failed. Inputs were {a} and {b}.");
+        }
+
+        [TestCase(10, 3, 7, TestName = "Substract_PositiveNumbers")]
+        [TestCase(5, 10, -5, TestName = "Substract_ResultMustNegative")]
+        [TestCase(-5, -5, 0, TestName = "Substract_NegativeNumbers")]
+        [TestCase(10.5, 2.5, 8.0, TestName = "Substract_DecimalNumbers")]
+        [TestCase(100, 0, 100, TestName = "Substract_WithZero")]
+        public void Should_GiveRightAnswer_When_SubstractDifferentInputs(double a, double b, double expectedResult)
+        {
+            // Arrange
+            var calculator = new Project.Calculator.Core.Calculator();
+
+            // Act
+            var result = calculator.Substract(a, b);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expectedResult), $"Failed. Inputs were {a} and {b}.");
+        }
+
+        [TestCase(5, 4, 20, TestName = "Multiply_PositiveNumbers")]
+        [TestCase(10, 0, 0, TestName = "Multiply_ByZero")]
+        [TestCase(-5, -4, 20, TestName = "Multiply_WithNegative")]
+        [TestCase(2.5, 2, 5, TestName = "Multiply_DecimalNumbers")]
+        public void Should_GiveRightAnswer_When_MultiplyTwoNumbers(double a, double b, double expectedResult)
+        {
+            // Arrange
+            var calculator = new Project.Calculator.Core.Calculator();
+
+            // Act
+            var result = calculator.Multiply(a, b);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expectedResult), $"Failed. Inputs were {a} and {b}.");
+        }
+
+        [TestCase(10, 2, 5, TestName = "Divide_PositiveNumbers")]
+        [TestCase(10, 4, 2.5, TestName = "Divide_ResultMustDecimal")]
+        [TestCase(-10, -2, 5, TestName = "Divide_NegativeNumbers")]
+        public void Should_GiveRightAnswer_When_DivideTwoNumbers(double a, double b, double expectedResult)
+        {
+            // Arrange
+            var calculator = new Project.Calculator.Core.Calculator();
+
+            // Act
+            var result = calculator.Divide(a, b);
+
+            // Arrange
+            Assert.That(result, Is.EqualTo(expectedResult), $"Failed. Inputs were {a} and {b}.");
+        }
+
+        [Test]
+        public void Should_ThrowException_When_DivideWithZero()
+        {
+            // Arrange
+            var calculator = new Project.Calculator.Core.Calculator();
+
+            // Act and Assert
+            Assert.Throws<DivideByZeroException>(() => calculator.Divide(10, 0));
+        }
+    }
+}
---
+++ Calculator.core/Calculator.cs
@@ -0,0 +1,35 @@
+﻿
+
+
+namespace Project.Calculator.Core
+{
+    public class Calculator
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+
+        }
+
+        public double Substract(double a, double b)
+        {
+            return a - b;
+        }
+
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Divide(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new System.DivideByZeroException("Cannot divide by zero!");
+            }
+
+            return a / b;
+        }
+    }
+}
